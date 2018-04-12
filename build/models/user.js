7231a7597c9b3478e7d16f6dd731b17ce2f337df
Tristan Matthias <=>
2018-04-12 23:54:56 +1000

    Added /setup routes

--- build/models/user.js
+++ build/models/user.js
@@ -4,7 +4,7 @@ module.exports = {
     properties: {
         id: 'uuid',
         fname: { type: String, required: true },
-        lname: { type: String, required: true },
+        lname: { type: String, required: false },
         email: { type: 'email', required: false, unique: true },
         password: { type: String, required: true, hidden: true }
     }
--- src/models/user.ts
+++ src/models/user.ts
@@ -3,7 +3,7 @@ module.exports = {
     properties: {
         id: 'uuid',
         fname: {type: String, required: true},
-        lname: {type: String, required: true},
+        lname: {type: String, required: false},
         email: {type: 'email', required: false, unique: true},
         password: {type: String, required: true, hidden: true}
     }
