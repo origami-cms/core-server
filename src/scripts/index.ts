edef816dfd64e8d54d02b15a2617c8efb9011614
Tristan Matthias <=>
2018-09-06 21:44:57 +1000

    Removed root user from start scripts

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -3,6 +3,6 @@ import rootUser from './rootUser';
 import initialConfig from './initialConfig';
 
 export default async(app: Server) => {
-    await rootUser(app);
+    // await rootUser(app);
     await initialConfig(app);
 };
