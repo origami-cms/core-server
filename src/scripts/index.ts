759c90f61f50e75c22339b825c05229a8d7bb557
Tristan Matthias <=>
2018-09-12 11:54:14 +1000

    Removed default scripts

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,8 +1,4 @@
 import Server from '../server';
-import rootUser from './rootUser';
-import initialConfig from './initialConfig';
 
 export default async(app: Server) => {
-    // await rootUser(app);
-    // await initialConfig(app);
 };
